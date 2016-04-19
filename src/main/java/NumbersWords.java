cc001971196e41ec66318c18f8fd52351635dbbf
ming wen and scott mcintire <mwen82@gmail.com>
2016-04-19 16:17:20 -0700

    adds tests for numbers 1-19

--- src/main/java/NumbersWords.java
+++ src/main/java/NumbersWords.java
@@ -5,28 +5,57 @@ import java.util.ArrayList;
 public class NumbersWords {
   public static void main(String[] args) {}
 
-  public static String calculateNumber(Integer userNumber) {
+  public static String calculateNumber(String userNumber) {
 
-    HashMap<Integer, String> singleDigits = new HashMap<Integer, String>();
-    singleDigits.put(1, "one");
-    singleDigits.put(2, "two");
-    singleDigits.put(3, "three");
-    singleDigits.put(4, "four");
-    singleDigits.put(5, "five");
-    singleDigits.put(6, "six");
-    singleDigits.put(7, "seven");
-    singleDigits.put(8, "eight");
-    singleDigits.put(9, "nine");
+    HashMap<String, String> singleDigits = new HashMap<String, String>();
+    singleDigits.put("1", "one");
+    singleDigits.put("2", "two");
+    singleDigits.put("3", "three");
+    singleDigits.put("4", "four");
+    singleDigits.put("5", "five");
+    singleDigits.put("6", "six");
+    singleDigits.put("7", "seven");
+    singleDigits.put("8", "eight");
+    singleDigits.put("9", "nine");
 
-    String converter = "";
-    for(int i = 0; i < singleDigits.size(); i++) {
-    //   if(userNumber == singleDigits.get("one")) {
-    //     converter = "one";
-    //   } else {
-    //
-    //   }
-    }
-    String results = singleDigits.get(userNumber);
+    HashMap<String, String> teens = new HashMap<String, String>();
+    teens.put("10", "ten");
+    teens.put("11", "eleven");
+    teens.put("12", "twelve");
+    teens.put("13", "thirteen");
+    teens.put("14", "fourteen");
+    teens.put("15", "fifteen");
+    teens.put("16", "sixteen");
+    teens.put("17", "seventeen");
+    teens.put("18", "eighteen");
+    teens.put("19", "nineteen");
+
+    HashMap<String, String> tensDigits = new HashMap<String, String>();
+    tensDigits.put("2", "twenty");
+    tensDigits.put("3", "thirty");
+    tensDigits.put("4", "forty");
+    tensDigits.put("5", "fifty");
+    tensDigits.put("6", "sixty");
+    tensDigits.put("7", "seventy");
+    tensDigits.put("8", "eighty");
+    tensDigits.put("9", "ninety");
+
+
+    Integer number = Integer.parseInt(userNumber);
+
+    char[] numberString = userNumber.toCharArray();
+
+    String results = new String();
+
+    if (number < 10) {
+    results = singleDigits.get(userNumber);
+  } else if (number > 10 && number < 20) {
+    results = teens.get(userNumber);
+  } else if (number > 19 && number < 100) {
+    results = (tensDigits.get(numberString[0]) + " " + singleDigits.get(numberString[1]));
+  } else {
+
+  }
     return results;
   }
 
--- src/test/java/NumbersWordsTest.java
+++ src/test/java/NumbersWordsTest.java
@@ -4,9 +4,22 @@ import static org.junit.Assert.*;
 public class NumbersWordsTest {
 
   @Test
-  public void calculateNumber_forNumberOne_1() {
+  public void calculateNumber_forNumberOneThroughNine_1() {
     NumbersWords numbersWords = new NumbersWords();
-    String expected = "nine";
-    assertEquals(expected, numbersWords.calculateNumber(9));
+    String expected = "one";
+    assertEquals(expected, numbersWords.calculateNumber("1"));
   }
+  @Test
+  public void calculateNumber_forNumberTenThroughNineteen_11() {
+    NumbersWords numbersWords = new NumbersWords();
+    String expected = "eleven";
+    assertEquals(expected, numbersWords.calculateNumber("11"));
+  }
+  @Test
+  public void calculateNumber_forNumberTwentyThroughNinetyNine_21() {
+    NumbersWords numbersWords = new NumbersWords();
+    String expected = "twenty one";
+    assertEquals(expected, numbersWords.calculateNumber("21"));
+  }
+
 }
